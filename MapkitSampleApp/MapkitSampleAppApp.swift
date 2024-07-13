33098649a3120378092c0083b92e39c89edbee38
Prathamesh Chakote <chakote@pdx.edu>
2024-07-13 14:58:18 -0700

    Initial Commit

---
+++ MapkitSampleApp/MapkitSampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapkitSampleAppApp.swift
+//  MapkitSampleApp
+//
+//  Created by Prathamesh on 7/13/24.
+//
+
+import SwiftUI
+
+@main
+struct MapkitSampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
