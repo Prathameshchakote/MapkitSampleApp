79f1a061fd43f3a033c5530a6fead0f47e1e680e
Prathamesh Chakote <chakote@pdx.edu>
2024-07-19 14:49:42 -0700

    Map kit integration

--- MapkitSampleApp/ContentView.swift
+++ MapkitSampleApp/ContentView.swift
@@ -6,16 +6,125 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    
+    @State private var cameraPosition: MapCameraPosition = .region(.userRegion)
+    @State private var searchtext: String = ""
+    @State private var result = [MKMapItem]()
+    @State private var mapSelection: MKMapItem?
+    @State private var showDetails = false
+    @State var getDirections: Bool = false
+    @State private var routeDisplaying = false
+    @State private var routeDestination: MKMapItem?
+    @State private var route: MKRoute?
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        Map(position: $cameraPosition, selection: $mapSelection){
+            Marker("My Position", systemImage: "paperplane" ,coordinate: .userloaction)
+                .tint(.blue)
+            
+            ForEach(result, id: \.self) { item in
+                
+                if getDirections {
+                    if item == routeDestination {
+                        let placemark = item.placemark
+                        Marker(placemark.name ?? "", coordinate: placemark.coordinate)
+                    }
+                } else {
+                    let placemark = item.placemark
+                    Marker(placemark.name ?? "", coordinate: placemark.coordinate)
+                }
+            }
+            
+            if let route {
+                MapPolyline(route.polyline)
+                    .stroke(.blue, lineWidth: 6)
+            }
+            
         }
-        .padding()
+        .overlay(alignment: .top){
+            TextField("Enter the loaction", text: $searchtext)
+                .font(.subheadline)
+                .padding(12)
+                .background(.white)
+                .padding()
+                .cornerRadius(9)
+                .shadow(radius: 12)
+        }
+        .onSubmit(of: .text) {
+            print("Search of location of query \(searchtext)")
+            Task {await searchPlaces()}
+        }
+        .onChange(of: mapSelection, { oldValue, newValue in
+            showDetails = newValue != nil
+        })
+        .onChange(of: getDirections, { oldValue, newValue in
+            if newValue {
+                fetchRoute()
+            }
+        })
+        .sheet(isPresented: $showDetails, content: {
+            LocationDetailView(mapSelection: $mapSelection, show: $showDetails, getDirections: $getDirections)
+                .presentationDetents([.height(340)])
+                .presentationBackgroundInteraction(.enabled(upThrough: .height(340)))
+                .presentationCornerRadius(12)
+        })
+        .mapControls {
+            MapCompass()
+//            MapPitchButton()
+            MapPitchToggle()
+            MapUserLocationButton()
+        }
+    }
+}
+
+extension ContentView {
+    func searchPlaces() async {
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = searchtext
+        request.region = .userRegion
+        
+        let result = try? await MKLocalSearch(request: request).start()
+        self.result = result?.mapItems ?? []
+    }
+    
+    func fetchRoute() {
+        if let mapSelection {
+            let request = MKDirections.Request()
+            request.source = MKMapItem(placemark: .init(coordinate: .userloaction))
+            request.destination = mapSelection
+            
+            Task {
+                let result = try? await MKDirections(request: request).calculate()
+                route = result?.routes.first
+                routeDestination = mapSelection
+                
+                withAnimation(.snappy) {
+                    routeDisplaying = true
+                    showDetails = false
+                    
+                    if let rect = route?.polyline.boundingMapRect, routeDisplaying {
+                        cameraPosition = .rect(rect)
+                    }
+                    
+                }
+            }
+        }
+    }
+}
+
+extension CLLocationCoordinate2D {
+    static var userloaction: CLLocationCoordinate2D {
+        return .init(latitude: 25.760, longitude: -80.1959)
+    }
+}
+
+extension MKCoordinateRegion {
+    static var userRegion: MKCoordinateRegion {
+        return .init(center: .userloaction, latitudinalMeters: 10000, longitudinalMeters: 10000)
     }
 }
 
--- MapkitSampleApp/LocationDetailView.swift
+++ MapkitSampleApp/LocationDetailView.swift
@@ -5,14 +5,103 @@
 //  Created by Prathamesh on 7/13/24.
 //
 
+import MapKit
 import SwiftUI
 
 struct LocationDetailView: View {
+    @Binding var mapSelection: MKMapItem?
+    @Binding var show: Bool
+    @State private var lookAroundScene: MKLookAroundScene?
+    @Binding var getDirections: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                VStack(alignment: .leading){
+                    Text(mapSelection?.placemark.name ?? "")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                    
+                    Text(mapSelection?.placemark.title ?? "")
+                        .font(.footnote)
+                        .foregroundStyle(.gray)
+                        .lineLimit(2)
+                        .padding(.trailing)
+                        .fontWeight(.semibold)
+                }
+                
+                Spacer()
+                
+                Button(action: {
+                    show.toggle()
+                    mapSelection = nil
+                }, label: {
+                    Image(systemName: "xmark.circle.fill")
+                        .resizable()
+                        .frame(width: 24, height: 24)
+                        .foregroundColor(Color(.systemGray6))
+                })
+            }
+            
+            if let scene = lookAroundScene {
+                LookAroundPreview(initialScene: scene)
+                    .frame(height: 200)
+                    .cornerRadius(12)
+                    .padding()
+            } else {
+                ContentUnavailableView("No Preview AValiable",systemImage: "eye.slash")
+            }
+            
+            HStack(spacing: 24) {
+                Button {
+                    if let mapSelection {
+                        mapSelection.openInMaps()
+                    }
+                }label: {
+                    Text ("Open in Maps")
+                        .font (.headline)
+                        .foregroundColor(.white)
+                        .frame(width: 170, height:48)
+                        .background (.green)
+                        .cornerRadius (12)
+                }
+                
+                Button {
+                    getDirections = true
+                    show = false
+                } label: {
+                    Text ("Get Directions")
+                        .font (.headline)
+                        .foregroundColor(.white)
+                        .frame(width: 170, height:48)
+                        .background (.blue)
+                        .cornerRadius (12)
+                }
+            }
+            .padding(.horizontal)
+        }
+        .onAppear(perform: {
+            fetchLookAroundPreview()
+        })
+        .onChange(of: mapSelection) { oldValue, newValue in
+            fetchLookAroundPreview()
+        }
+        .padding()
+    }
+}
+
+extension LocationDetailView {
+    func fetchLookAroundPreview() {
+        if let mapSelection {
+            lookAroundScene = nil
+            Task {
+                let request = MKLookAroundSceneRequest(mapItem: mapSelection)
+                lookAroundScene = try? await request.scene
+            }
+        }
     }
 }
 
 #Preview {
-    LocationDetailView()
+    LocationDetailView(mapSelection: .constant(nil), show: .constant(false), getDirections: .constant(false))
 }
