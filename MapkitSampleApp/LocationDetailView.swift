6c320bfa9e242be64d3621d802eef77de95246fb
Prathamesh Chakote <chakote@pdx.edu>
2024-07-19 14:48:33 -0700

    Mapkit sample application

---
+++ MapkitSampleApp/LocationDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationDetailView.swift
+//  MapkitSampleApp
+//
+//  Created by Prathamesh on 7/13/24.
+//
+
+import SwiftUI
+
+struct LocationDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationDetailView()
+}
